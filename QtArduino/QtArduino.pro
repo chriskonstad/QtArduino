8de3b40bf658206b47de4135abe741feb04e62e2
Chris Konstad <chriskon149@gmail.com>
2012-07-25 23:40:48 -0700

    Added base files for Qt application (fully functional) Added code to ignore

---
+++ QtArduino/QtArduino.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-07-25T22:14:49
+#
+#-------------------------------------------------
+
+
+#Must add the "network" tag to include the UDP classes
+QT       += core gui network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QtArduino
+TEMPLATE = app
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ QtArduino/mainwindow.cpp
@@ -0,0 +1,69 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = new QUdpSocket(this);  //Create an instance of the QUdpSocket class pointed to by socket
+    socket->bind(51000, QUdpSocket::ShareAddress);  //make the socket listen on port 51000 and make it try to share the port with any other program the OS is running
+    packetTimer = new QTimer(this); //Create an instance of the QTimer class pointed to by packetTimer
+    packetTimer->setInterval(100);   //have it send a packet 10x a second
+    connect(packetTimer, SIGNAL(timeout()), this, SLOT(sendPacket()));  //When the timer runs out, send a packet
+    packetTimer->start();   //Start the timer
+    connect(socket, SIGNAL(readyRead()), this, SLOT(onPacketReceived()));   //When a packet is received, call onPacketRecieved()
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onPacketReceived()
+{
+    QString packet;
+    QByteArray datagram;
+    int digital;
+    int analog;
+
+    do
+    {
+        datagram.resize(socket->pendingDatagramSize());
+        socket->readDatagram(datagram.data(), datagram.size());
+    }
+    while(socket->hasPendingDatagrams());
+    packet = (tr("\"%1\"").arg(datagram.data()));   //turn the datagram into a string
+    packet.remove(QChar('"'), Qt::CaseInsensitive); //remove quotation marks
+
+    QTextStream stream(&packet);    //create a textstream to parse the packet
+
+    stream >> digital >> analog;    //parse the packet
+
+    //Change the color of the QLineEdit
+    if(digital == 1)
+        ui->leDigital->setStyleSheet("QLineEdit { background-color: rgb(136, 195, 240) }");
+    else
+        ui->leDigital->setStyleSheet("QLineEdit { background-color: none }");
+
+    ui->barAnalog->setValue(analog); //slide the progress bar
+
+}
+
+void MainWindow::sendPacket()
+{
+    QString packet; //create the packet string to hold the values to send
+
+    if(ui->pbDigital->isDown())
+        packet.append(QString::number(1));
+    else
+        packet.append(QString::number(0));
+    packet.append(" "); //deliminate the packet with whitespace
+    packet.append(QString::number(ui->hsAnalog->value()));
+
+    QByteArray datagram = packet.toAscii(); //convert into a datagram
+
+    socket->writeDatagram(datagram, QHostAddress::Broadcast, 50000); //braodcast the packet on port 50000
+
+    packetTimer->start();   //restart the timer
+}
